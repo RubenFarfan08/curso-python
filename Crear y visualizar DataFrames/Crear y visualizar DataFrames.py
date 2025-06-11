508f4c1b8bc29d9c5ec52c6ec3c23fa1c784b1dd
ruben Farfan <ruben081293@outlook.com>
2025-06-11 13:58:16 -0600

    se termina curso de manipulacion de datos con pandas

--- Crear y visualizar DataFrames/Crear y visualizar DataFrames.py
+++ Crear y visualizar DataFrames/Crear y visualizar DataFrames.py
@@ -151,10 +151,60 @@ avocados_filled[cols_with_missing].hist()
 plt.show()
 
 #example of dictionary
-# Create a dictionary with the data
-data = {
-    "name": ["Alice", "Bob", "Charlie"],
-    "age": [25, 30, 35],
-    "city": ["New York", "Los Angeles", "Chicago"]
+# Create a list of dictionaries with new data
+avocados_list = [
+    {"date": "2025-06-11", "small_sold": 15, "large_sold": 20},
+    {"date": "2025-06-10", "small_sold": 25, "large_sold": 200},
+]
+
+# Convert list into DataFrame
+avocados_2019 = pd.DataFrame(avocados_list)
+
+# Create a list of dictionaries with new data
+avocados_list = [
+    {"date": "2019-11-03", "small_sold": 10376832, "large_sold": 7835071},
+    {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
+]
+
+# Convert list into DataFrame
+avocados_2019 = pd.DataFrame(avocados_list)
+
+# Print the new DataFrame
+print(avocados_2019)
+
+# Create a dictionary of lists with new data
+avocados_dict = {
+  "date": ["2019-11-17","2019-12-01"],
+  "small_sold": [10859987,9291631],
+  "large_sold": [7674135,6238096]
 }
 
+# Convert dictionary into DataFrame
+avocados_2019 = pd.DataFrame(avocados_dict)
+# Print the new DataFrame
+print(avocados_2019)
+
+# Ejercicios de csv a dataframe
+# Read CSV as DataFrame called airline_bumping
+airline_bumping = pd.read_csv("airline_bumping.csv")
+
+# Take a look at the DataFrame
+print(airline_bumping.head())
+
+# From previous step
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+
+# For each airline, select nb_bumped and total_passengers and sum
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped","total_passengers"]].agg(sum)
+# Calcular pasajeros accidentados por 10k
+airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"] * 10000
+
+# Print airline_totals
+print(airline_totals)
+
+# ordena airline_totals por bumps_per_10k de mayor a menor
+airline_totals_sorted = airline_totals.sort_values("bumps_per_10k", ascending=False)
+
+# Save as airline_totals_sorted.csv
+airline_totals_sorted.to_csv("airline_totals_sorted.csv")
